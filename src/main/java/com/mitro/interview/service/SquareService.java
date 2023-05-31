987ce4af4f46974b74b4846232f7af321ea399b3
williamjmitro <havoc315@gmail.com>
2023-05-31 15:52:36 -0400

    Adding new code

--- src/main/java/com/mitro/interview/service/SquareService.java
+++ src/main/java/com/mitro/interview/service/SquareService.java
@@ -4,27 +4,37 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntBinaryOperator;
 import java.util.stream.Collectors;
 
 @Service
-public class SquareService
-{
-    public List<Integer> findSquares(String line)
-    {
+public class SquareService {
+    public List<Integer> findSquares(String line) {
         String[] rawInput = line.split(" ");
-        List<Integer> input = Arrays.stream(rawInput)
-                                    .map(Integer::valueOf)
-                                    .collect(Collectors.toList());
-
-        return input.stream()
-                    .filter(integer -> checkForPerfect(integer))
-                    .sorted()
-                    .collect(Collectors.toList());
-
+        return Arrays.stream(rawInput)
+                .map(Integer::valueOf)
+                .filter(SquareService::checkForPerfect)
+                .sorted()
+                .collect(Collectors.toList());
     }
 
-    public static boolean checkForPerfect(int input)
-    {
+    public List<Integer> findEvensAndSquare(List<Integer> input, IntBinaryOperator operator, Integer numberToUse) {
+
+        return input.stream()
+                .map(integer -> integer % 2 == 0 ? operator.applyAsInt(integer, numberToUse) : integer)
+                .sorted()
+                .toList();
+    }
+
+    public List<Integer> findOddsAndMultiple(List<Integer> inputList) {
+
+        return inputList.stream()
+                .map(integer -> integer % 2 == 1 ? integer / 3 : integer)
+                .sorted()
+                .toList();
+    }
+
+    public static boolean checkForPerfect(int input) {
         int num = ((int) Math.sqrt(input));
 
         return num * num == input;
--- src/test/java/com/mitro/interview/service/SquareServiceTest.java
+++ src/test/java/com/mitro/interview/service/SquareServiceTest.java
@@ -9,24 +9,41 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.function.IntBinaryOperator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = InterviewApplication.class)
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-public class SquareServiceTest
-{
+public class SquareServiceTest {
 
     @Autowired
     SquareService squareService;
 
     @Test
-    public void testApp()
-    {
-        String        input      = "1 33 4 16 9 55 2";
+    public void testApp() {
+        String input = "1 33 4 16 9 55 2";
         List<Integer> sortedList = squareService.findSquares(input);
 
         assertEquals(sortedList.toString(), "[1, 4, 9, 16]");
+    }
+
+    /**
+     * All odds should return empty list
+     * All evens should return a list of the same size as the input
+     * <p>
+     * <p>
+     * Rewrote this test to make use of the new requirement.
+     */
+    @Test
+    public void testEvens() {
+        List<Integer> testList = List.of(4);
+
+        IntBinaryOperator expression = (left, right) -> left / right;
+
+        List<Integer> sortedList = squareService.findEvensAndSquare(testList, expression, 4);
+
+        assertEquals(sortedList.toString(), "[1]");
     }
 }
